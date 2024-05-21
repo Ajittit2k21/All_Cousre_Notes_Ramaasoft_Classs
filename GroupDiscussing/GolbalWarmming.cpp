2382f47ccac2f93e124c2efffdb17e491ae04e6a
Ajit kumar <152725006+Ajittit2k21@users.noreply.github.com>
2024-05-21 19:48:34 +0530

    Add files via upload

---
+++ GroupDiscussing/GolbalWarmming.cpp
@@ -0,0 +1,26 @@
+Explain Global Warmming
+
+1.Global wramming is a critical problem in the world today;
+
+2.Global wramming is a phenomenon in which the earths average temprature issues;
+
+3.There are many reasons of global warmming like green-houses gases.
+	deforestation,pollution etc.
+	
+4.Greenhouse gasses such as carbon dioxide and methane play a vital role in causing global 
+	warmming
+	
+5.Global warmming brings extreme climate change and results in floods, droughts and other
+	climate disosters.
+	
+6.Global warmming 1 causes melting of glociers and rise in sea level;
+
+7.Global warmming brings rise to many dangerous diseases and epidemic as well
+8.Global warmming has a negative impact on all kinds of life including humans,birds,plants,
+	and animals.
+9.Plantation of trees,reduced use fo fossil fuels,controlled vehicle and industrial pollution
+	are the solutions for global wramming
+10.Global warmming will bring disas-ter for us. it is time to be serious about it. 
+
+  
+	
---
+++ GroupDiscussing/IndoorOutdoorGames.cpp
@@ -0,0 +1,63 @@
+Importance of Outdoor Games
+
+1.Playing outdoor games like cricket,football, basketball,and badmintion are known to benefit
+our physical and mental health.
+
+2.It improves blood flow,strengthens our muscles,improves our immunity and stamina,relaxes
+	our body and mind, helps us stay foucsed and keeps us fit.
+
+3.Playing outdoors also allows us to get some sunlight which in turn helps the body produce
+	vitamin and strengthens our bones.
+	
+4.spending time in nature refreshes our mood and keeps us happy.
+
+5.Playing together with other people also teaches us social skills and helps us understand the
+	meaning and value of team spirit trus and working together as, a unit.
+	
+6.Outdoor games help in the overall growth and development of children and also keep people
+	of all ages healthy.
+	
+	
+Benefits of Outdoor games:
+1.it gives them an opportunity to learn new things .
+2.It can help in their physical development.
+3.it can boost their creativity.
+4.it can help the them acquire social skills.
+5.it can help them acquire social skills.
+6.it can help them develoment a positive attitude.
+7.it helps in personality development.
+8.It helps improvement the attention span in children.
+9.It strengthens their motor skills.
+10.it makes them lead a healthy lifestyle.
+
+---------------------Indoor Games---------------------------------
+1.Games that are played inside a door are called indoor games.
+2. Indoor games can be played at home, in the room or in the hall.
+3. The biggest advantage of  thes gamaes is that they are not  affected by bad weather.
+4.They can be played indivdually or by team.
+5.They body does not get tire due to not having much physical activity in these sports.
+6.They require less equipment as compared to outdoor sports.
+7.These games are chess,cardgames and carrom board etc.
+8.Indoor games increases creativity.
+9.Many indoor games have a bad effect on the eyes like playing video games for a  long
+ time.
+10.My favourite indoor games is ludo.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+
+
