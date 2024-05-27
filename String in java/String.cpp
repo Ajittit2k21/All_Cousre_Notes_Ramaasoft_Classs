090964038cb5d9c07d9ff353c21723bc4e89f3ac
Ajit kumar <152725006+Ajittit2k21@users.noreply.github.com>
2024-05-27 07:13:49 +0530

    Add files via upload

---
+++ String in java/String.cpp
@@ -0,0 +1,11 @@
+String in java:
+	
+	Its group/sequences zero or mroe characters
+	it is represented by using ""
+	In java String its represented internal as class
+	
+	*responsible classes are
+	*String
+	*StringBuffer
+	*StringBuilder
+	*StringTokenizer
