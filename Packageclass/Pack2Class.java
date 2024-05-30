582b1defea12d356c79978f390424328e0a0a255
Ajit kumar <152725006+Ajittit2k21@users.noreply.github.com>
2024-05-30 08:25:01 +0530

    Add files via upload

---
+++ Packageclass/Pack2Class.java
@@ -0,0 +1,5 @@
+ package pack1;
+public class PackClass{
+	public static void main(String[]args){
+
+		System.out.println("Hello new package floder");
---
+++ Packageclass/pack3Class.java
@@ -0,0 +1,8 @@
+package jain;
+class pack3Class{
+	public static void main(String[]args){
+		System.out.println("Hello");
+
+	}
+
+}
